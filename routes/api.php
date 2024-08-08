046e07817f622b7047001fe6fc492a5ba3ddc1ed
Bharathamuthan <srbharath911@gmail.com>
2024-08-08 13:35:12 +0530

    folders add

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,27 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::post('registration','Api\CustomController@registration');
 Route::post('login','Api\CustomController@login');
+//Customer Table
+Route::post('customermeasurement','Api\CustomController@customermeasurement');
+Route::get('getAllCustomers','Api\CustomController@getAllCustomers');
+Route::get('getCustomer/{id}','Api\CustomController@getCustomer');
+Route::get('editCustomer/{id}','Api\CustomController@editCustomer');
+Route::get('deleteCustomer/{id}','Api\CustomController@deleteCustomer');
+Route::post('updateCustomer/{id}','Api\CustomController@updateCustomer');
+//Tailor Shop user
+Route::post('tailorshopuser','Api\CustomController@tailorshopuser');
+Route::get('getAllTailorusers','Api\CustomController@getAllTailorusers');
+Route::get('getTailoruser/{id}','Api\CustomController@getTailoruser');
+Route::get('editTailoruser/{id}','Api\CustomController@editTailoruser');
+Route::get('deleteTailoruser/{id}','Api\CustomController@deleteTailoruser');
+Route::post('updateTailoruser/{id}','Api\CustomController@updateTailoruser');
+//Order List
+Route::post('orderlist','Api\CustomController@orderlist');
+//Order Type
+Route::post('ordertype','Api\CustomController@ordertype');
+
 Route::post('password/email','Api\CustomController@forgotPassword');
 Route::post('logout','Api\CustomController@logoutApi');
 Route::post('password/reset', 'Api\PasswordResetController@passwordReset');
